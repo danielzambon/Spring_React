9badeccd70ef404263f324e9c11ca4f9f3777786
danielzambon <danielfzambon@gmail.com>
2022-07-17 12:27:04 -0300

    DatePicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,3 +1,5 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton'
 import './styles.css'
 
@@ -8,17 +10,19 @@ function SalesCard() {
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
                         className="dsmeta-form-control"
-                        type="text"
-                        value="01/02/2022"
+                        dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
                         className="dsmeta-form-control"
-                        type="text"
-                        value="30/04/2022"
+                        dateFormat="dd/MM/yyyy"
                     />
                 </div>
             </div>
@@ -39,10 +43,10 @@ function SalesCard() {
                         <tr>
                             <td className="tc992">#341</td>
                             <td className="tc576">28/06/2022</td>
-                            <td>Anakin</td>
-                            <td className="tc992">15</td>
-                            <td className="tc992">11</td>
-                            <td>R$ 55300.00</td>
+                            <td>Daniel</td>
+                            <td className="tc992">09</td>
+                            <td className="tc992">30</td>
+                            <td>R$ 3500.00</td>
                             <td>
                                 <div className="dsmeta-btn-container">
                                     <div className="dsmeta-red-btn">
