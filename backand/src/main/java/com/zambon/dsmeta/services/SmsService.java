0ea9655251eb4f665535054779816e013bc63968
danielzambon <danielfzambon@gmail.com>
2022-07-17 16:16:19 -0300

    twilio SMS

---
+++ backand/src/main/java/com/zambon/dsmeta/services/SmsService.java
@@ -0,0 +1,51 @@
+package com.zambon.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import com.zambon.dsmeta.entities.Sale;
+import com.zambon.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long id) {
+
+		Sale sale = saleRepository.findById(id).get();
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+		String msg = "O vendedor " + sale.getSeller_name() 
+				+ " foi destaque em " 
+				+ date + " com um TOTAL de R$ " 
+				+ String.format("%.2f", sale.getAmount());
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+
+}
